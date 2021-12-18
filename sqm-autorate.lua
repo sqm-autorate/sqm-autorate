2e5761fae133ed8f42ed69ba8002f5ba33263d9c
Fail-Safe <Fail-Safe@users.noreply.github.com>
2021-12-17 20:34:52 -0500

    Added reflector_array_v6 addresses for test server reflectors.

--- sqm-autorate.lua
+++ sqm-autorate.lua
@@ -33,7 +33,8 @@ local min_change_interval = 0.5 -- don't change speeds unless this many seconds 
 --                            "149.112.112.11", "149.112.112.112", "193.19.108.2", "193.19.108.3", "9.9.9.9", "9.9.9.10",
 --                            "9.9.9.11"}
 local reflector_array_v4 = {"65.21.108.153", "5.161.66.148", "216.128.149.82", "108.61.220.16"}
-local reflector_array_v6 = {"2620:fe::10", "2620:fe::fe:10"} -- TODO Implement IPv6 support?
+local reflector_array_v6 = {"2a01:4f9:c010:5469::1", "2a01:4ff:f0:2194::1", "2001:19f0:5c01:1bb6:5400:03ff:febe:3fae",
+                            "2001:19f0:6001:3de9:5400:03ff:febe:3f8e"}
 
 local alpha_owd_increase = 0.001 -- how rapidly baseline OWD is allowed to increase
 local alpha_owd_decrease = 0.9 -- how rapidly baseline OWD is allowed to decrease
