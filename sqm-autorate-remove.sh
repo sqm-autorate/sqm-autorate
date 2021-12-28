3709bbdd9d94261cb37379a9887ebeb0a2eee244
Fail-Safe <Fail-Safe@users.noreply.github.com>
2021-12-28 15:07:17 -0500

    Added remove script.

---
+++ sqm-autorate-remove.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+name="sqm-autorate"
+autorate_root="/usr/lib/sqm-autorate"
+
+echo ">>> Disabling and stopping service..."
+/etc/init.d/"$name" disable
+/etc/init.d/"$name" stop
+sleep 3
+echo ">>> Removing service file..."
+rm -f /etc/init.d/"$name"
+echo ">>> Removing sqm-autorate lib directory..."
+rm -rf "$autorate_root"
+echo ">>> Removing config file..."
+rm -f /etc/config/"$name"
+
+echo "!!! If you would like to remove the Lua modules which this setup previously installed, please run the following command:"
+echo "!!! WARNING: Only run the following if no other applications are using these Lua modules..."
+echo "luarocks remove vstruct && opkg remove luarocks lua-bit32 luaposix lualanes"
