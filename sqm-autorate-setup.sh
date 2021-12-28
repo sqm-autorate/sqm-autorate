d1f1f63f535f0def65d6b5cc5d10b5758650db76
Fail-Safe <Fail-Safe@users.noreply.github.com>
2021-12-28 15:16:48 -0500

    Fix bad path regression.

--- sqm-autorate-setup.sh
+++ sqm-autorate-setup.sh
@@ -65,13 +65,13 @@ if [ -f "$owrt_release_file" ]; then
             if [ "$is_git_proj" = true ]; then
                 cp "./config/$config_file" "/etc/config/$name-NEW"
             else
-                mv "./config/$config_file" "/etc/config/$name-NEW"
+                mv "./$config_file" "/etc/config/$name-NEW"
             fi
         else
             if [ "$is_git_proj" = true ]; then
                 cp "./config/$config_file" "/etc/config/$name"
             else
-                mv "./config/$config_file" "/etc/config/$name"
+                mv "./$config_file" "/etc/config/$name"
             fi
         fi
     fi
@@ -82,14 +82,14 @@ mkdir -p "$autorate_root"
 if [ "$is_git_proj" = true ]; then
     cp "./lib/$lua_file" "$autorate_root/$lua_file"
 else
-    mv "./lib/$lua_file" "$autorate_root/$lua_file"
+    mv "./$lua_file" "$autorate_root/$lua_file"
 fi
 
 echo ">>> Putting service file into place..."
 if [ "$is_git_proj" = true ]; then
     cp "./service/$service_file" "/etc/init.d/$name"
 else
-    mv "./service/$service_file" "/etc/init.d/$name"
+    mv "./$service_file" "/etc/init.d/$name"
 fi
 chmod a+x "/etc/init.d/$name"
 
