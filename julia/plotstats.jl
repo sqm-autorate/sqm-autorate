967f4d745f7987e53e112896bc657e0894b84f37
Daniel Lakeland <dlakelan@lappy1.street-artists.org>
2022-01-05 11:43:48 -0800

    Allow easy zooming of the timeseries

--- julia/plotstats.jl
+++ julia/plotstats.jl
@@ -15,18 +15,22 @@ downmax = maximum(dat.dlrate)
 upmax = maximum(dat.uprate)
 
 
-pload = @df dat plot(:times + :timens/1e9,:rxload,title="Bandwidth Fractional utilization",xlab="time (s)",ylab="Relative load/delay",labels="DL Load",legend=:topright,xlim=xlimvals,ylim=(0,1.25))
-@df dat plot!(:times + :timens/1e9,:txload,xlab="time (s)",ylab="Relative load",labels="UL Load")
-
-pdel = @df dat plot(:times + :timens/1e9, :deltadelaydown,title="Delay through time", label="Down Delay",ylab="delay (ms)",xlim=(xlimvals))
-@df dat plot!(:times + :timens/1e9, :deltadelayup,label="Up Delay")
-
-pbw = @df dat plot(:times + :timens/1e9,:dlrate ./ 1000,title="Cake Bandwidth Setting",label="Download Rate (Mbps)",xlab="time (s)",ylab="Mbps",legend=:topright,xlim=xlimvals,ylim=(0,max(downmax,upmax)*1.2/1000.0))
-@df dat plot!(:times + :timens/1e9,:uprate ./ 1000,label="Upload Rate (Mbps)",xlab="time (s)",ylab="Mbps")
-
-plot(pload,pdel,pbw,layout=grid(3,1,heights=[.45,.1,.45]),size=(800,1200),left_margin=5mm)
-savefig("timeseries.png")
-
+function plotts(dat,filename,xlimvals)
+    
+    pload = @df dat plot(:times + :timens/1e9,:rxload,title="Bandwidth Fractional utilization",xlab="time (s)",ylab="Relative load/delay",labels="DL Load",legend=:topright,xlim=xlimvals,ylim=(0,1.25))
+    @df dat plot!(:times + :timens/1e9,:txload,xlab="time (s)",ylab="Relative load",labels="UL Load")
+    
+    pdel = @df dat plot(:times + :timens/1e9, :deltadelaydown,title="Delay through time", label="Down Delay",ylab="delay (ms)",xlim=(xlimvals))
+    @df dat plot!(:times + :timens/1e9, :deltadelayup,label="Up Delay")
+    
+    pbw = @df dat plot(:times + :timens/1e9,:dlrate ./ 1000,title="Cake Bandwidth Setting",label="Download Rate (Mbps)",xlab="time (s)",ylab="Mbps",legend=:topright,xlim=xlimvals,ylim=(0,max(downmax,upmax)*1.2/1000.0))
+    @df dat plot!(:times + :timens/1e9,:uprate ./ 1000,label="Upload Rate (Mbps)",xlab="time (s)",ylab="Mbps")
+    
+    plot(pload,pdel,pbw,layout=grid(3,1,heights=[.45,.1,.45]),size=(800,1200),left_margin=5mm)
+    savefig(filename)
+end
+plotts(dat,filename) = plotts(dat,filename,xlimvals)
+plotts(dat,"timeseries.png")
 
 hists = CSV.read(histfile,DataFrame)
 
@@ -54,3 +58,9 @@ savefig("delaydownecdf.png")
              title="Fraction of Up delay less than x",xlab="delay (ms)",ylab="Fraction")
 plot!([0.0,50.0],[relthr,relthr],label="1 second per hour",legend=:bottomright)
 savefig("delayupecdf.png")
+
+
+## plot a timeseries zoomed to a particular region:
+
+plotts(dat,"zoomedts.png",(0,100))
+
