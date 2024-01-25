1165dbdc44594c9aa2c1e23d835092007b9b66cb
Mark Baker <mark@vpost.net>
2024-01-25 00:33:06 -0500

    Bring back lua5.1 package.loaders

--- lib/utility.lua
+++ lib/utility.lua
@@ -82,7 +82,8 @@ function M.is_module_available(name)
     if package.loaded[name] then
         return true
     else
-        for _, searcher in ipairs(package.searchers) do
+        ---@diagnostic disable-next-line: deprecated
+        for _, searcher in ipairs(package.searchers or package.loaders) do
             local loader = searcher(name)
             if type(loader) == 'function' then
                 package.preload[name] = loader
